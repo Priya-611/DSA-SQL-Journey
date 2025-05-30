68c1cae33acda5891fb6a778f4df330f46c37820
Priya-611 <149269977+Priya-611@users.noreply.github.com>
2025-05-30 23:13:00 +0530

    Update Day1.cpp
     This file contains a collection of basic C++ programs demonstrating core concepts of the language.
    It includes well-commented examples and practice problems covering:
    
     Printing output and using variables
    
     Arithmetic operations with increment/decrement
    
     Typecasting and ASCII conversions
    
     Conditional statements (if-else, switch)
    
     Ternary operator usage
    
     Loops: for, while, do-while, and range-based for-each
    
     Pattern printing and basic logic building exercises

--- DSA/Day1.cpp
+++ DSA/Day1.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main(){
     cout<<"HELLO WORLD!!"<<endl;
     cout<<"We are practising dsa using cpp";
+
+    return 0;
 }
 
 
@@ -17,6 +19,8 @@ int main(){
     cout<<"x*y: "<<x<<" * " <<++y<<" = "<<x*y<<endl;
     cout<<"x/y: "<<x<<" / " <<y<<" = "<<x/y<<endl;
     cout<<"x%y: "<<--x<<" % " <<y<<" = "<<x%y<<endl;
+
+    return 0;
 }
 
 
@@ -44,6 +48,182 @@ int main(){
     cin>>z;
     cout<<"Character value of "<<z<<" is "<<(char)z<<endl;
 
+    return 0;
+
 }
+
+
+
+//if-else
+
+#include<iostream>
+using namespace std;
+int main(){
+    int amount;
+    int balance=10000;
+    cout<<"Enter the amount you want to withdraw: ";
+    cin>>amount;
+    if(amount <=0) cout<<"Enter a valid amount "<<endl;
+    else if(amount%100==0) cout<<"Amount must be multiple of 100"<<endl;
+    else if(amount>balance) cout<<"Insufficeint balance"<<endl;
+    else{
+        balance-=amount;
+        cout<<"Withdrawal successfull"<<endl;
+        cout<<"Current balance: "<<balance<<endl;
+    }
+    return 0;
+}
+
+
+
+//switch 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter x and y with a space: ";
+    cin>>x>>y;
+    cout<<"Enter the symbol(+, -, /, *, %): ";
+    char ch;
+    cin>>ch;
+    switch (ch){
+        case '+':
+            cout<<x+y<<endl;
+            break;
+        case '-':
+            cout<<x-y<<endl;
+            break;
+        case '*':
+            cout<<x*y<<endl;
+            break;
+        case '/':
+            cout<<x/y<<endl;
+            break;
+        case '%':
+            cout<<x%y<<endl;
+            break;
+        default:
+            cout<<"invalid operator";
+    }
+    return 0;
+}
+
+
+
+//Ternary operator 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter the marks scored: ";
+    cin>>marks;
+    marks>=40?cout<<"Pass":cout<<"Fail"<<endl;
+}
+
+
+//for loop
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+//while loop
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num=60;
+    int x;
+    cout<<"Guess the number: ";
+    cin>>x;
+    while(num!=x){
+        
+        if(x<num){
+            cout<<"Too low!! "<<endl<<"Try again: ";
+        }
+        else if(x>num){
+            cout<<"Too high!!"<<endl<<"Try again: ";    
+        }       
+        
+        cin>>x;
+    }
+    cout<<"You guesssed it right!!"<<endl<<"The number is "<<x<<endl;
+            
+    return 0;
+}
+
+
+
+//do-while
+
+#include<iostream>
+using namespace std;
+int main(){
+   string username="admin" ;
+   string pass="1234";
+   string u,p;
+   do{
+    cout<<"Enter username: ";
+    cin>>u;
+
+    cout<<endl<<"Enter password: ";
+    cin>>p;
+
+    if(username!=u || pass!=p){
+        cout<<"Invalid credentials. Try again!!";
+    }
+   }
+   while(username!=u || pass!=p);
+   cout<<"Login successfull!!"<<endl;
+
+   return 0;
+}
+
+
+
+//for-each loop
+
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of students: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter marks: "<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    double sum=0;
+    for(int n: arr){
+        sum+=n;
+    }
+    double avg=sum/n;
+    cout<<"Average marks: "<<fixed<<setprecision(2)<<avg<<endl;
+
+    return 0;
+}
+
 
 
