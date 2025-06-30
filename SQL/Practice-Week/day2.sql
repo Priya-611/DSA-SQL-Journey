3add0a6bbd5d5bc0a0c6459f6c15ff524758722c
Priya-611 <149269977+Priya-611@users.noreply.github.com>
2025-06-30 21:23:27 +0530

    Update day2.sql

--- SQL/Practice-Week/day2.sql
+++ SQL/Practice-Week/day2.sql
@@ -1,1 +1,83 @@
 
+-- 1757. Recyclable and Low Fat Products
+-- Products table:
+-- +-------------+----------+------------+
+-- | product_id  | low_fats | recyclable |
+-- +-------------+----------+------------+
+-- | 0           | Y        | N          |
+-- | 1           | Y        | Y          |
+-- | 2           | N        | Y          |
+-- | 3           | Y        | Y          |
+-- | 4           | N        | N          |
+-- +-------------+----------+------------+  
+select product_id from Products where low_fats=='Y' and recyclable=='Y';
+
+
+
+
+-- 584. Find Customer Referee
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+select name from Customer where referee_id != 2 or referee_id is null;
+
+
+
+
+
+-- 595. Big Countries
+-- World table:
+-- +-------------+-----------+---------+------------+--------------+
+-- | name        | continent | area    | population | gdp          |
+-- +-------------+-----------+---------+------------+--------------+
+-- | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+-- | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+-- | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+-- | Andorra     | Europe    | 468     | 78115      | 3712000000   |
+-- | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
+-- +-------------+-----------+---------+------------+--------------+
+select name, population,area from World where area >= 3000000 or population >=25000000;
+
+
+
+
+
+
+-- 1148. Article Views I
+--   Views table:
+-- +------------+-----------+-----------+------------+
+-- | article_id | author_id | viewer_id | view_date  |
+-- +------------+-----------+-----------+------------+
+-- | 1          | 3         | 5         | 2019-08-01 |
+-- | 1          | 3         | 6         | 2019-08-02 |
+-- | 2          | 7         | 7         | 2019-08-01 |
+-- | 2          | 7         | 6         | 2019-08-02 |
+-- | 4          | 7         | 1         | 2019-07-22 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- +------------+-----------+-----------+------------+
+select distinct author_id as id from Views where author_id= viewer_id order by author_id;
+
+
+
+
+
+-- 1683. Invalid Tweets
+--   Tweets table:
+-- +----------+-----------------------------------+
+-- | tweet_id | content                           |
+-- +----------+-----------------------------------+
+-- | 1        | Let us Code                       |
+-- | 2        | More than fifteen chars are here! |
+-- +----------+-----------------------------------+
+select tweet_id from Tweets where length(content)>15;
+
+
