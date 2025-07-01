f5538f4d79449a11e31a3a7f1cddd854806a4fb3
Priya-611 <149269977+Priya-611@users.noreply.github.com>
2025-07-02 00:04:34 +0530

    Update day3.sql

--- SQL/Practice-Week/day3.sql
+++ SQL/Practice-Week/day3.sql
@@ -1,1 +1,255 @@
 
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+-- Visits
+-- +----------+-------------+
+-- | visit_id | customer_id |
+-- +----------+-------------+
+-- | 1        | 23          |
+-- | 2        | 9           |
+-- | 4        | 30          |
+-- | 5        | 54          |
+-- | 6        | 96          |
+-- | 7        | 54          |
+-- | 8        | 54          |
+-- +----------+-------------+
+-- Transactions
+-- +----------------+----------+--------+
+-- | transaction_id | visit_id | amount |
+-- +----------------+----------+--------+
+-- | 2              | 5        | 310    |
+-- | 3              | 5        | 300    |
+-- | 9              | 5        | 200    |
+-- | 12             | 1        | 910    |
+-- | 13             | 2        | 970    |
+-- +----------------+----------+--------+
+--using left join because we need all data from visit even if there's no trnsaction
+--WHERE t.transaction_id IS NULL: Filters to only visits with no transactions.
+--GROUP BY v.customer_id: Count how many times each customer made such "no transaction" visits.
+  
+select v.customer_id ,count(v.visit_id) as count_no_trans 
+from Visits v left join Transactions t
+on v.visit_id=t.visit_id
+where transaction_id is null
+group by v.customer_id;
+
+
+
+
+
+
+
+-- 1661. Average Time of Process per Machine
+-- Input: 
+-- Activity table:
+-- +------------+------------+---------------+-----------+
+-- | machine_id | process_id | activity_type | timestamp |
+-- +------------+------------+---------------+-----------+
+-- | 0          | 0          | start         | 0.712     |
+-- | 0          | 0          | end           | 1.520     |
+-- | 0          | 1          | start         | 3.140     |
+-- | 0          | 1          | end           | 4.120     |
+-- | 1          | 0          | start         | 0.550     |
+-- | 1          | 0          | end           | 1.550     |
+-- | 1          | 1          | start         | 0.430     |
+-- | 1          | 1          | end           | 1.420     |
+-- | 2          | 0          | start         | 4.100     |
+-- | 2          | 0          | end           | 4.512     |
+-- | 2          | 1          | start         | 2.500     |
+-- | 2          | 1          | end           | 5.000     |
+-- +------------+------------+---------------+-----------+
+-- Output: 
+-- +------------+-----------------+
+-- | machine_id | processing_time |
+-- +------------+-----------------+
+-- | 0          | 0.894           |
+-- | 1          | 0.995           |
+-- | 2          | 1.456           |
+-- +------------+-----------------+
+-- Explanation: 
+-- There are 3 machines running 2 processes each.
+-- Machine 0's average time is ((1.520 - 0.712) + (4.120 - 3.140)) / 2 = 0.894
+-- Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
+-- Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
+
+select a1.machine_id, round(avg(a2.timestamp - a1.timestamp),3)  as processing_time
+from Activity a1 join Activity a2 
+on a1.machine_id=a2.machine_id
+and a1.process_id=a2.process_id
+and a1.activity_type='start'
+and a2.activity_type='end'
+group by a1.machine_id;
+
+
+
+
+
+
+
+
+-- 1280. Students and Examinations
+-- Input: 
+-- Students table:
+-- +------------+--------------+
+-- | student_id | student_name |
+-- +------------+--------------+
+-- | 1          | Alice        |
+-- | 2          | Bob          |
+-- | 13         | John         |
+-- | 6          | Alex         |
+-- +------------+--------------+
+-- Subjects table:
+-- +--------------+
+-- | subject_name |
+-- +--------------+
+-- | Math         |
+-- | Physics      |
+-- | Programming  |
+-- +--------------+
+-- Examinations table:
+-- +------------+--------------+
+-- | student_id | subject_name |
+-- +------------+--------------+
+-- | 1          | Math         |
+-- | 1          | Physics      |
+-- | 1          | Programming  |
+-- | 2          | Programming  |
+-- | 1          | Physics      |
+-- | 1          | Math         |
+-- | 13         | Math         |
+-- | 13         | Programming  |
+-- | 13         | Physics      |
+-- | 2          | Math         |
+-- | 1          | Math         |
+-- +------------+--------------+
+-- Output: 
+-- +------------+--------------+--------------+----------------+
+-- | student_id | student_name | subject_name | attended_exams |
+-- +------------+--------------+--------------+----------------+
+-- | 1          | Alice        | Math         | 3              |
+-- | 1          | Alice        | Physics      | 2              |
+-- | 1          | Alice        | Programming  | 1              |
+-- | 2          | Bob          | Math         | 1              |
+-- | 2          | Bob          | Physics      | 0              |
+-- | 2          | Bob          | Programming  | 1              |
+-- | 6          | Alex         | Math         | 0              |
+-- | 6          | Alex         | Physics      | 0              |
+-- | 6          | Alex         | Programming  | 0              |
+-- | 13         | John         | Math         | 1              |
+-- | 13         | John         | Physics      | 1              |
+-- | 13         | John         | Programming  | 1              |
+-- +------------+--------------+--------------+----------------+
+-- Explanation: 
+-- The result table should contain all students and all subjects.
+-- Alice attended the Math exam 3 times, the Physics exam 2 times, and the Programming exam 1 time.
+-- Bob attended the Math exam 1 time, the Programming exam 1 time, and did not attend the Physics exam.
+-- Alex did not attend any exams.
+-- John attended the Math exam 1 time, the Physics exam 1 time, and the Programming exam 1 time.
+
+
+-- CROSS JOIN: Ensures each student is paired with every subject.
+-- LEFT JOIN Examinations: Finds matching exams (if any) for each (student, subject) pair.
+select s.student_id , s.student_name, sub.subject_name, count(e.student_id) as attended_exams
+from Students s 
+cross join Subjects sub 
+left join Examinations e
+on s.student_id=e.student_id and sub.subject_name=e.subject_name
+group by s.student_id,s.student_name,sub.subject_name
+order by s.student_id;
+
+
+
+
+
+
+
+-- 620. Not Boring Movies
+--   Cinema table:
+-- +----+------------+-------------+--------+
+-- | id | movie      | description | rating |
+-- +----+------------+-------------+--------+
+-- | 1  | War        | great 3D    | 8.9    |
+-- | 2  | Science    | fiction     | 8.5    |
+-- | 3  | irish      | boring      | 6.2    |
+-- | 4  | Ice song   | Fantacy     | 8.6    |
+-- | 5  | House card | Interesting | 9.1    |
+-- +----+------------+-------------+--------+
+
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+select * from Cinema where id%2!=0 and description!="boring"
+order by rating desc;
+
+
+
+
+
+
+
+
+-- 1251. Average Selling Price
+-- Prices table:
+-- +------------+------------+------------+--------+
+-- | product_id | start_date | end_date   | price  |
+-- +------------+------------+------------+--------+
+-- | 1          | 2019-02-17 | 2019-02-28 | 5      |
+-- | 1          | 2019-03-01 | 2019-03-22 | 20     |
+-- | 2          | 2019-02-01 | 2019-02-20 | 15     |
+-- | 2          | 2019-02-21 | 2019-03-31 | 30     |
+-- +------------+------------+------------+--------+
+-- UnitsSold table:
+-- +------------+---------------+-------+
+-- | product_id | purchase_date | units |
+-- +------------+---------------+-------+
+-- | 1          | 2019-02-25    | 100   |
+-- | 1          | 2019-03-01    | 15    |
+-- | 2          | 2019-02-10    | 200   |
+-- | 2          | 2019-03-22    | 30    |
+-- +------------+---------------+-------+
+
+-- we'll find avg of price and fill 0 if it is null(left join because UnitsSold can be empty)
+-- date must be b/w start and end date
+select p.product_id ,round(ifnull(sum(p.price*u.units)/ sum(u.units),0),2) as average_price 
+from Prices p left join UnitsSold u 
+on p.product_id=u.product_id 
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
+
+
+
+
+
+
+
+
+-- 1075. Project Employees I
+-- Project table:
+-- +-------------+-------------+
+-- | project_id  | employee_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- | 1           | 2           |
+-- | 1           | 3           |
+-- | 2           | 1           |
+-- | 2           | 4           |
+-- +-------------+-------------+
+-- Employee table:
+-- +-------------+--------+------------------+
+-- | employee_id | name   | experience_years |
+-- +-------------+--------+------------------+
+-- | 1           | Khaled | 3                |
+-- | 2           | Ali    | 2                |
+-- | 3           | John   | 1                |
+-- | 4           | Doe    | 2                |
+-- +-------------+--------+------------------+
+-- Output: 
+-- +-------------+---------------+
+-- | project_id  | average_years |
+-- +-------------+---------------+
+-- | 1           | 2.00          |
+-- | 2           | 2.50          |
+-- +-------------+---------------+
+
+select p.project_id, round(avg(e.experience_years),2)average_years 
+from Project p join Employee e
+on p.employee_id=e.employee_id
+group by p.project_id
+order by p.project_id;
