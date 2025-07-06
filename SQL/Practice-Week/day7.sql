e8c9ea977555760f8d28e639fcc9d25c851d0bf2
Priya-611 <149269977+Priya-611@users.noreply.github.com>
2025-07-06 23:11:15 +0530

    Update day7.sql

--- SQL/Practice-Week/day7.sql
+++ SQL/Practice-Week/day7.sql
@@ -1,1 +1,209 @@
 
+-- 1193. Monthly Transactions I
+-- Input: 
+-- Transactions table:
+-- +------+---------+----------+--------+------------+
+-- | id   | country | state    | amount | trans_date |
+-- +------+---------+----------+--------+------------+
+-- | 121  | US      | approved | 1000   | 2018-12-18 |
+-- | 122  | US      | declined | 2000   | 2018-12-19 |
+-- | 123  | US      | approved | 2000   | 2019-01-01 |
+-- | 124  | DE      | approved | 2000   | 2019-01-07 |
+-- +------+---------+----------+--------+------------+
+-- Output: 
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+-- | 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+-- | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+
+-- (using 'Y' for getting only 2 digits in year part | using 'y' we get all four digit in year part)
+select date_format(trans_date,'%Y-%m') as month, country ,
+  count(*) as trans_count, 
+  sum(case when state ='approved' then 1 else 0 end) as approved_count, 
+  sum(amount) as trans_total_amount, 
+  sum(case when state ='approved' then amount else 0 end) as approved_total_amount from Transactions
+group by date_format(trans_date,'%Y-%m'),country;
+
+
+
+
+
+
+
+
+-- 1174. Immediate Food Delivery II
+--   Input: 
+-- Delivery table:
+-- +-------------+-------------+------------+-----------------------------+
+-- | delivery_id | customer_id | order_date | customer_pref_delivery_date |
+-- +-------------+-------------+------------+-----------------------------+
+-- | 1           | 1           | 2019-08-01 | 2019-08-02                  |
+-- | 2           | 2           | 2019-08-02 | 2019-08-02                  |
+-- | 3           | 1           | 2019-08-11 | 2019-08-12                  |
+-- | 4           | 3           | 2019-08-24 | 2019-08-24                  |
+-- | 5           | 3           | 2019-08-21 | 2019-08-22                  |
+-- | 6           | 2           | 2019-08-11 | 2019-08-13                  |
+-- | 7           | 4           | 2019-08-09 | 2019-08-09                  |
+-- +-------------+-------------+------------+-----------------------------+
+-- Output: 
+-- +----------------------+
+-- | immediate_percentage |
+-- +----------------------+
+-- | 50.00                |
+-- +----------------------+
+-- Explanation: 
+-- The customer id 1 has a first order with delivery id 1 and it is scheduled.
+-- The customer id 2 has a first order with delivery id 2 and it is immediate.
+-- The customer id 3 has a first order with delivery id 5 and it is scheduled.
+-- The customer id 4 has a first order with delivery id 7 and it is immediate.
+-- Hence, half the customers have immediate first orders.
+
+
+select round(avg( order_date=customer_pref_delivery_date)*100,2) as immediate_percentage 
+from Delivery 
+where (customer_id,order_date) 
+in(select customer_id, min(order_date) from Delivery group by customer_id);
+
+
+
+
+
+
+
+-- 1141. User Activity for the Past 30 Days I
+--   Input: 
+-- Activity table:
+-- +---------+------------+---------------+---------------+
+-- | user_id | session_id | activity_date | activity_type |
+-- +---------+------------+---------------+---------------+
+-- | 1       | 1          | 2019-07-20    | open_session  |
+-- | 1       | 1          | 2019-07-20    | scroll_down   |
+-- | 1       | 1          | 2019-07-20    | end_session   |
+-- | 2       | 4          | 2019-07-20    | open_session  |
+-- | 2       | 4          | 2019-07-21    | send_message  |
+-- | 2       | 4          | 2019-07-21    | end_session   |
+-- | 3       | 2          | 2019-07-21    | open_session  |
+-- | 3       | 2          | 2019-07-21    | send_message  |
+-- | 3       | 2          | 2019-07-21    | end_session   |
+-- | 4       | 3          | 2019-06-25    | open_session  |
+-- | 4       | 3          | 2019-06-25    | end_session   |
+-- +---------+------------+---------------+---------------+
+-- Output: 
+-- +------------+--------------+ 
+-- | day        | active_users |
+-- +------------+--------------+ 
+-- | 2019-07-20 | 2            |
+-- | 2019-07-21 | 2            |
+-- +------------+--------------+ 
+select activity_date as day,count(distinct user_id) as active_users from Activity
+where activity_date>"2019-06-27" and activity_date<="2019-07-27"
+group by activity_date;
+
+
+
+
+
+
+-- 1070. Product Sales Analysis III
+--   Input: 
+-- Sales table:
+-- +---------+------------+------+----------+-------+
+-- | sale_id | product_id | year | quantity | price |
+-- +---------+------------+------+----------+-------+ 
+-- | 1       | 100        | 2008 | 10       | 5000  |
+-- | 2       | 100        | 2009 | 12       | 5000  |
+-- | 7       | 200        | 2011 | 15       | 9000  |
+-- +---------+------------+------+----------+-------+
+
+-- Output: 
+-- +------------+------------+----------+-------+
+-- | product_id | first_year | quantity | price |
+-- +------------+------------+----------+-------+ 
+-- | 100        | 2008       | 10       | 5000  |
+-- | 200        | 2011       | 15       | 9000  |
+-- +------------+------------+----------+-------+
+
+select product_id,year as first_year,quantity,price from Sales
+where (product_id,year) in(
+  select product_id,min(year) as yr from Sales 
+  group by product_id
+  );
+
+
+
+
+
+
+
+-- 1045. Customers Who Bought All Products
+-- Input: 
+-- Customer table:
+-- +-------------+-------------+
+-- | customer_id | product_key |
+-- +-------------+-------------+
+-- | 1           | 5           |
+-- | 2           | 6           |
+-- | 3           | 5           |
+-- | 3           | 6           |
+-- | 1           | 6           |
+-- +-------------+-------------+
+-- Product table:
+-- +-------------+
+-- | product_key |
+-- +-------------+
+-- | 5           |
+-- | 6           |
+-- +-------------+
+-- Output: 
+-- +-------------+
+-- | customer_id |
+-- +-------------+
+-- | 1           |
+-- | 3           |
+-- +-------------+
+
+select distinct customer_id from Customer 
+group by customer_id
+having count(distinct product_key)=
+  (select count(distinct product_key) from Product );
+
+
+
+
+
+
+-- 180. Consecutive Numbers
+--   Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
+-- (join Logs table three time on l1.id and l2.id has gap of 1 | l1.id and l3.id has gap of 2 and the num is same)
+select distinct l1.num as ConsecutiveNums 
+from Logs l1 
+join 
+Logs l2 on l1.id=l2.id-1
+join 
+Logs l3 on l1.id=l3.id-2
+where l1.num=l2.num and l2.num=l3.num;
+
+
