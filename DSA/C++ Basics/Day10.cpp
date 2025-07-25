db6b27d4c7e1be02ae49bf1009ef6a6ef389d590
Priya Rawat <149269977+Priya-611@users.noreply.github.com>
2025-07-25 21:31:53 +0530

    Update Day10.cpp

--- DSA/C++ Basics/Day10.cpp
+++ DSA/C++ Basics/Day10.cpp
@@ -273,3 +273,27 @@ public:
     }
 };
 
+
+
+
+
+
+
+// 344. Reverse String
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            char temp=s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;
+            r--;
+        }
+    }
+};
+
