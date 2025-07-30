3c90b7307389ad04e3dac35292068d5695951346
Priya Rawat <149269977+Priya-611@users.noreply.github.com>
2025-07-30 22:48:08 +0530

    Update Day 5.cpp

--- DSA/Hashing/Day 5.cpp
+++ DSA/Hashing/Day 5.cpp
@@ -25,6 +25,7 @@ If num is not present, it returns 0.*/
 
 
 
+
 // 771. Jewels and Stones
 // Input: jewels = "aA", stones = "aAAbbbb"
 // Output: 3
@@ -41,6 +42,40 @@ public:
 };
 
 
+
+
+
+
+// 49. Group Anagrams
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+        for(string s:strs){
+            string x = s;
+            sort(x.begin(),x.end());   //sort each string that we get
+            mp[x].push_back(s);       //and store all original string value in the sorted key [such that string with same char get together]
+        }
+        vector<vector<string>>res;
+        for(auto &p:mp){
+            res.push_back(p.second);   //to get any the values i.e. the grouped anagram
+        }
+        return res;
+    }
+};
+
+/*
+| Word | Sorted | Group added to map     |
+| ---- | ------ | ---------------------- |
+| eat  | aet    | \["eat"]               |
+| tea  | aet    | \["eat", "tea"]        |
+| tan  | ant    | \["tan"]               |
+| ate  | aet    | \["eat", "tea", "ate"] |
+| nat  | ant    | \["tan", "nat"]        |
+| bat  | abt    | \["bat"]               |
+*/
 
 
 
